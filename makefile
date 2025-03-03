24426821d25968732c0e854e661001280963e3f7
Gothloverino <alino0203@gmail.com>
2025-03-03 19:01:22 +0100

    test push_swap

---
+++ makefile
@@ -0,0 +1,3 @@
+# Purpose: Makefile for push_swap
+all:
+	gcc -Wall -Wextra -Werror push_swap.c -o push_swap
---
+++ push_swap.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void init_stack_a(int argc, char **argv, int *stack_a)
+{
+	int i;
+
+	i = 0;
+	while(i < argc - 1)
+	{
+		stack_a[i] = atoi(argv[i + 1]);
+		i++;
+	}
+	stack_a[i] = 0;
+}
+
+void print_stack(int argc, int *stack_a)
+{
+	int i;
+
+	i = 0;
+	while (i < argc - 1)
+	{
+		printf("%d\n", stack_a[i]);
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int stack_a[100];
+	//int stack_b[100];
+
+	init_stack_a(argc, argv, stack_a);
+	print_stack(argc, stack_a);
+}

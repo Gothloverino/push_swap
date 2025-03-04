4a0dea97fcac04c2bacc61b2dd3beef3ecbdf877
Gothloverino <alino0203@gmail.com>
2025-03-04 16:27:20 +0100

    part2

--- operatorions.c
+++ operatorions.c
@@ -6,6 +6,66 @@ void    swap(int *stack)
 	stack[0] = stack[1];
 	stack[1] = recipient;
 }
+void shift(int *stack, int size, int direction)
+{
+	int i;
+	int recipient;
+
+	if (direction > 1)
+	{
+		size++;
+		realloc(stack, size);
+		recipient = stack[1];
+		i = size;
+		while (i > 0)
+		{
+			stack[i] = stack[i - 1];
+			i--;
+		}
+		stack[i] = recipient;
+	}
+	else if (direction < 1)
+	{
+		size--;
+		realloc(stack, size);
+		recipient = stack[0];
+		i = 0;
+		while (i < size)
+		{
+			stack[i] = stack[i + 1];
+			i++;
+		}
+		stack[i] = recipient;
+	}
+}
+
+void	push_a(int *stack_a, int *stack_b,int size_a,int size_b)
+{
+	if (size_b != 0)
+	{	
+		int recipient;
+		int recipient_stack;
+		int i;
+
+		shift(stack_a, size_a, 1);
+		stack_a[0] = stack_b[0];
+		shift(stack_a, size_b, -1);
+	}
+}
+
+void	push_b(int *stack_a, int *stack_b,int size_a,int size_b)
+{
+	if (size_b != 0)
+	{	
+		int recipient;
+		int recipient_stack;
+		int i;
+
+		shift(stack_b, size_b, 1);
+		stack_b[0] = stack_a[0];
+		shift(stack_a, size_a, -1);
+	}
+}
 
 void	rotate(int *stack, int argc)
 {
